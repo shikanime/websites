347d28791bed0f26de26976c829d6acc2d51f723
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-09-01 12:05:08 +0000

    Refactor font handling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
       },
     },
   },
